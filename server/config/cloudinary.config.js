f140d04d6e05f8689c4f6d4d8e9cfab1369fd7c0
maanasmahato4 <maanasmahato456@gmail.com>
2023-11-21 23:17:56 +0545

    env

--- server/config/cloudinary.config.js
+++ server/config/cloudinary.config.js
@@ -1,9 +1,9 @@
 const cloudinary = require("cloudinary").v2;
 
 cloudinary.config({
-    cloud_name: 'dqn88yr6y',
-    api_key: '825141699117774',
-    api_secret: 'XlrXxTRQSHisQKwVeUyKexg-SFI',
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET,
     secure: true
 });
 
--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require('express');
 const { ErrorHandler } = require("./utils/ErrorHandler");
 const cors = require("cors");
